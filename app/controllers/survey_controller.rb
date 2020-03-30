8474659a9dcf086cfdc60a4281f833a768ce2b11
Matt Williams <matt.seongjun@gmail.com>
2020-03-30 15:45:22 -0400

    Rubocop fixes

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
-require "auth_code_validator"
+
+require 'auth_code_validator'
 
 class SurveyController < ApplicationController
   def show
-    if params[:id] == "index" && params[:cell_id]
+    if params[:id] == 'index' && params[:cell_id]
       auth_code_validator = AuthCodeValidator.new(3557, 25)
-      render "explain" if auth_code_validator.compute_rid(params[:auth_code]).valid?
-      render "index"
+      render 'explain' if auth_code_validator.compute_rid(params[:auth_code]).valid?
+      render 'index'
     else
       render params[:id] # plain: "Hello #{params[:id]}"
     end
@@ -21,9 +22,9 @@ class SurveyController < ApplicationController
   # no_permission -- no mic permission
   # mic_check -- perform mic check
   # no_sound -- no sound
-  # instructions -- next to learn to rate
-  # rate -- explains the buttons
+  # all_set -- next to learn to rate
+  # instructions -- explains the buttons
   # show_code -- gives code to enter at survey
-  # page8 -- next on both
-  # page9 -- thanks for rating
+  # rate -- next on both
+  # thankyou -- thanks for rating
 end
