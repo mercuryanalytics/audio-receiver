b8dbd58b425cdc8f2ff7efb264a293460bbcfb5f
Matt Williams <matt.seongjun@gmail.com>
2020-03-31 12:01:55 -0400

    Add method to encode new authcode

--- lib/auth_code_validator.rb
+++ lib/auth_code_validator.rb
@@ -1,24 +1,19 @@
 class AuthCodeValidator
   attr_accessor :c, :p, :n
 
-  A = "A".ord
+  A = 'A'.ord
 
   def initialize(c, p)
     @c = c
     @p = p
     @n = p + 1
-    raise ArgumentError, "Params are not coprime" if c.gcd(n * n * n) != 1
+    raise ArgumentError, 'Params are not coprime' if c.gcd(n * n * n) != 1
   end
 
   def valid?(auth_code)
     codes = auth_code.upcase.codepoints.map { |cp| cp - A }
     check = codes.pop
-    sum = codes.each_with_index.reduce(0) do |acc, (cp, i)|
-      factor = 2 - (i % 2)
-      addend = factor * cp
-      acc + (addend / n).floor + addend % n
-    end
-    return false if check != n - 1 - (sum % n)
+    return false if check != n - 1 - (check_sum(codes) % n)
 
     true
   end
@@ -29,6 +24,20 @@ class AuthCodeValidator
     hash = codes.reduce { |acc, cp| acc * n + cp }
     codes.reverse!
     mdiv(hash - p, c, n * n * n)
+  end
+
+  def compute_auth_code(rid)
+    hash = (rid * c + p) % (n * n * n)
+
+    codes = []
+    3.times do
+      codes << hash % n
+      hash /= n
+    end
+    sum = check_sum(codes)
+    codes.reverse!
+    codes << (n - 1 - (sum % n))
+    codes.map(&:chr).join('')
   end
 
   private
@@ -43,7 +52,8 @@ class AuthCodeValidator
       t, t_n = [t_n, t - q * t_n]
       r, r_n = [r_n, r - q * r_n]
     end
-    return t + m if t < 0
+    return t + m if t.negative?
+
     t
   end
 
@@ -51,5 +61,13 @@ class AuthCodeValidator
     a %= m
     inv = minv(b, m)
     (((inv * a) % m) + m) % m
+  end
+
+  def check_sum(codes)
+    codes.each_with_index.reduce(0) do |acc, (cp, i)|
+      factor = 2 - (i % 2)
+      addend = factor * cp
+      acc + (addend / n).floor + addend % n
+    end
   end
 end
