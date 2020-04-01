4c1d56a346d20c35153dce0617b627992c78085a
Scott Brickner <scottb@brickner.net>
2020-04-01 15:09:21 -0400

    Fix deploy configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-server "audio-receiver.us-east-1", user: "deployer", roles: %w[app app db]
+server "audio-receiver.us-east-1", user: "deployer", roles: %w[app web db]
 
 set :ssh_options, forward_agent: true
