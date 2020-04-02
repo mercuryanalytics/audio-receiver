7f99433e2c2099a954dc096bbbd1daa0772de793
Matt Williams <matt.seongjun@gmail.com>
2020-04-02 15:20:41 -0400

    Bring survey in line with new spec
    - Modify survey text
    - Show/hide text while mic checking
    - Log user-agent when setting rid
    - Check and set rid as a before_action

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -3,18 +3,18 @@
 require 'auth_code_validator'
 
 class SurveyController < ApplicationController
+  before_action :check_rid
+
   def show
     @invalid = params[:invalid]
     session[:cell] = params[:cell] if params[:cell] && !session[:cell]
-    if valid?(params[:code])
-      render 'explain'
-    elsif session[:rid].nil?
-      render 'index'
-    elsif params[:id] == 'show_code'
+
+    case params[:id]
+    when 'show_code'
       auth_code_validator = AuthCodeValidator.new(3593, 25)
       @code = auth_code_validator.compute_auth_code(session[:rid])
       render 'show_code'
-    elsif params[:id] == 'no_sound'
+    when 'no_sound'
       session[:rid] = 0
       redirect_to survey_path(id: :show_code)
     else
@@ -37,9 +37,8 @@ class SurveyController < ApplicationController
   # index -- asks for authorization code
   # explain -- next to check mic
   # no_permission -- no mic permission
-  # mic_check -- perform mic check
-  # no_audio -- no audio above threshold
-  # no_sound -- no audio equipment
+  # no_audio -- redirect to show_code with rid = 0
+  # no_sound -- redirect to show_code with rid = 0
   # all_set -- next to learn to rate
   # instructions -- explains the buttons
   # show_code -- gives code to enter at survey
@@ -51,9 +50,18 @@ class SurveyController < ApplicationController
   def valid?(auth_code)
     auth_code_validator = AuthCodeValidator.new(3557, 25)
     if auth_code && auth_code_validator.valid?(auth_code)
-      session[:rid] = auth_code_validator.compute_rid(auth_code) if auth_code
+      session[:rid] = auth_code_validator.compute_rid(auth_code)
+      Rails.logger.info(request.headers['User-Agent'])
       return true
     end
     false
+  end
+
+  def check_rid
+    if valid?(params[:code])
+      redirect_to survey_path(id: :explain) # need window.location.href to be correct
+    elsif session[:rid].nil?
+      render 'index'
+    end
   end
 end
