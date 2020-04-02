6eef4258218c84aa5b4d54fbc24889d0b05ffaf1
Matt Williams <matt.seongjun@gmail.com>
2020-04-02 15:20:24 -0400

    Show/hide text while doing mic check

--- app/javascript/mic_check_ui.ts
+++ app/javascript/mic_check_ui.ts
@@ -5,6 +5,15 @@ const micCheckUi = () => {
   const micButton = document.querySelector<HTMLLinkElement>("#mic_check")
 
   if (micButton) {
+    micButton.addEventListener("click", () => {
+      const pretestText = document.getElementById("pretest")
+      const testingText = document.getElementById("testing")
+      if (pretestText && testingText) {
+        pretestText.style.display = "none"
+        testingText.style.display = "block"
+      }
+    })
+
     micButton.addEventListener("mic_check", (event: MicEvent) => {
       let target: string = event.detail
       if (target === "success") target = micButton.href
