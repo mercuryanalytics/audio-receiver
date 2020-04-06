3eb8b9eb5b1256d0acbe028fb4385a97ddb21717
Scott Brickner <scottb@brickner.net>
2020-04-06 14:47:39 -0400

    Tweak webpacker config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
+environment.config.set("performance.maxEntryPointSize", 512000)
+environment.config.set("performance.maxAssetSize", 512000)
+
 environment.loaders.prepend('typescript', typescript)
 module.exports = environment
