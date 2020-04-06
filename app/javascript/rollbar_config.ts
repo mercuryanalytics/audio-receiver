b60d637dcea171a87ade1397d10d9bba3b3e1ca2
Scott Brickner <scottb@brickner.net>
2020-04-06 17:51:25 -0400

    Remove debug

--- app/javascript/rollbar_config.ts
+++ app/javascript/rollbar_config.ts
@@ -3,7 +3,6 @@ import * as Rollbar from "rollbar";
 const meta = (name: string) => document.querySelector<HTMLMetaElement>(`meta[name='${name}']`).content;
 
 const environment = process.env.RAILS_ENV;
-console.log("env", environment);
 export default new Rollbar({
   enabled: environment === "production",
   accessToken: "086ed6a0d2f14f30aa2fa02588ef150f",
