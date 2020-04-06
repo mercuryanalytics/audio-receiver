57b9154ddad40683b11f336281f90beda6c9fe79
Scott Brickner <scottb@brickner.net>
2020-04-06 14:48:45 -0400

    More config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
-environment.config.set("performance.maxEntryPointSize", 512000)
+environment.config.set("performance.maxEntrypointSize", 512000)
 environment.config.set("performance.maxAssetSize", 512000)
 
 environment.loaders.prepend('typescript', typescript)
