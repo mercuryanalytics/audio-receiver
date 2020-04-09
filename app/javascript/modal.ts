7c1febc3a82a10dc1eda963460a71a887b683d8f
Scott Brickner <scottb@brickner.net>
2020-04-09 11:00:24 -0400

    Prevent button default on close

--- app/javascript/modal.ts
+++ app/javascript/modal.ts
@@ -1,22 +1,27 @@
 const initModal = () => {
-  const modal = document.querySelector('.modal') as HTMLElement
+  const modal = document.querySelector(".modal") as HTMLElement;
   if (modal) {
     const chooseModalContent = () => {
       if (Math.random() < 0.5) {
-        (document.querySelector(".modal .option-1") as HTMLElement).style.display = "none"
+        (document.querySelector(
+          ".modal .option-1"
+        ) as HTMLElement).style.display = "none";
       } else {
-        (document.querySelector(".modal .option-2") as HTMLElement).style.display = "none"
+        (document.querySelector(
+          ".modal .option-2"
+        ) as HTMLElement).style.display = "none";
       }
-    }
+    };
 
-    const closeModal = () => {
-      modal.style.display = 'none'
-      modal.querySelector('.close').removeEventListener('click', closeModal)
-    }
+    const closeModal = (e: MouseEvent) => {
+      e.preventDefault();
+      modal.style.display = "none";
+      modal.querySelector(".close").removeEventListener("click", closeModal);
+    };
 
-    chooseModalContent()
-    modal.querySelector('.close').addEventListener('click', closeModal)
+    chooseModalContent();
+    modal.querySelector(".close").addEventListener("click", closeModal);
   }
-}
+};
 
-document.addEventListener("DOMContentLoaded", initModal)
+document.addEventListener("DOMContentLoaded", initModal);
